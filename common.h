3724c93f394e1bdbed0fc085aa1ec9006236535e
OmarAzizi <oalazizi75@gmail.com>
2024-02-07 08:46:27 +0300

    Resolved conflicts and merged changes from remote

---
+++ common.h
@@ -0,0 +1,34 @@
+#ifndef COMMON_H
+#define COMMON_H
+
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#include <errno.h>
+#include <fcntl.h>
+#include <sys/time.h>
+#include <sys/ioctl.h>
+#include <netdb.h>
+#include <syscall.h>
+
+#define EXIT_SUCCESS   0
+#define EXIT_FALIURE   1
+
+#define MAXLINE       4096
+#define SA struct sockaddr
+#define LISTENQ       1024
+
+void handle_errors(const char*, ...);
+char* bin2hex(const unsigned char*, size_t);
+void Listen(int, int);
+
+#endif
